dcccf2c3a07d7a8847e553c4bc7d0cc659948f2f
zyhou <richard.maxime9@gmail.com>
2017-03-06 16:18:07 +0100

    add param folder name for saving pit

--- launcher.py
+++ launcher.py
@@ -2,9 +2,9 @@ import argparse
 from picture import picture
 
 
-def process_image(save):
+def process_image(save, numberPebble):
     image = picture.get_image_webcam()
-    image = picture.transform_board(image, save)
+    image = picture.transform_board(image, numberPebble, save)
     picture.display_picture(image)
 
 
@@ -16,8 +16,9 @@ if __name__ == "__main__":
     ap = argparse.ArgumentParser()
     ap.add_argument("-o", "--out")
     ap.add_argument("-s", "--save", action='store_true')
+    ap.add_argument("-n", "--number")
     args = vars(ap.parse_args())
     if args["out"] == "image":
-        process_image(args["save"])
+        process_image(args["save"], args["number"])
     elif args["out"] == "video":
         process_video()
--- picture/picture.py
+++ picture/picture.py
@@ -1,6 +1,7 @@
 import numpy as np
 import cv2
-
+import uuid
+import os
 
 def get_image_webcam():
     cam = cv2.VideoCapture(1)
@@ -10,8 +11,10 @@ def get_image_webcam():
     return image
 
 
-def transform_board(image, save=False):
-    path = "./images/"
+def transform_board(image, numberPebble, save=False):
+    path = "./images/{}".format(numberPebble)
+    if not os.path.exists(path):
+        os.makedirs(path)
 
     gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     cimg = cv2.medianBlur(gray, 5)
@@ -21,30 +24,35 @@ def transform_board(image, save=False):
     if circles is None:
         return False
 
-    edges = cv2.Canny(gray, 50, 200)
-    lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50,
-                            minLineLength=150, maxLineGap=10)
-    if lines is None:
-        return False
-
-    x1, y1, x2, y2 = lines[0][0]
-    cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    # edges = cv2.Canny(gray, 50, 200)
+    # lines = cv2.HoughLinesP(edges, 1, np.pi/180, 50,
+    #                         minLineLength=150, maxLineGap=10)
+    # if lines is None:
+    #     return False
+    #
+    # x1, y1, x2, y2 = lines[0][0]
+    # cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
 
     circles = np.round(circles[0, :]).astype("int")
-    # averageX = np.average(circles[:, 0])
-    # averageY = np.average(circles[:, 1])
-    # print(circles)
-    # print(averageX)
-    # print(averageY)
-
     for (x, y, r) in circles:
             cv2.circle(image, (x, y), r, (0, 255, 0), 2)
             cv2.circle(image, (x, y), 2, (0, 0, 255), 3)
+
+            crop_image = crop_picture(image, x, y)
             if save:
-                cv2.imwrite("{}{}.png".format(path, x), image)
+                cv2.imwrite(os.path.join(path, "{}.png".format(uuid.uuid1())),
+                            crop_image)
 
     return image
 
+
+def crop_picture(image, x, y):
+    cropSize = (100, 100)
+    cropCoords = (max(0, y-cropSize[0]//2),
+                  min(image.shape[0], y+cropSize[0]//2),
+                  max(0, x-cropSize[1]//2),
+                  min(image.shape[1], x+cropSize[1]//2))
+    return image[cropCoords[0]:cropCoords[1], cropCoords[2]:cropCoords[3]]
 
 def display_picture(image):
         cv2.imshow("output transform", image)
@@ -55,10 +63,7 @@ def transform_video():
     cap = cv2.VideoCapture(1)
 
     while(True):
-        # Capture frame-by-frame
         ret, frame = cap.read()
-
-        # Display the resulting frame
         cv2.imshow('frame', transform_board(frame))
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
